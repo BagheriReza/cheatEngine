f10693a5120e7ba9dbc6ace8fcfbb25b42dec8d1
Bagherireza <m.mrbagheri@gmail.com>
2022-03-01 05:00:52 +0330

    Health Point is added

--- ReadByPointers_AssaultCube/rpmAndwpm.cpp
+++ ReadByPointers_AssaultCube/rpmAndwpm.cpp
@@ -6,22 +6,29 @@
 #include <iomanip>
 #include <sstream> //for std::stringstream 
 #include <string>  //for std::string
+#include <tlhelp32.h>
+
 
 using namespace std;
 // functions declaration
 DWORD findingProcessId();
 DWORD findingFinalPointer(int Pointerdepth, HANDLE hProcess, DWORD offsets[], DWORD BaseAddress);
 bool readFromMemory(HANDLE hProcess, DWORD address);
-bool writeToMemory(HANDLE hProcess, DWORD address);
+bool writeToMemory(HANDLE hProcess, DWORD address, int value);
+bool updateAmmo(HANDLE hProcess, int value);
+bool updateHealthPoint(HANDLE hProcess, int value);
 
 //Global variable
 std::string nameOfGame = "AssaultCube";
 
 // Ammo based address, offsets, target value
-DWORD AmmoBaseAddress = { 0x0057B0B8 }; // Currently the playerBaseAddress
+DWORD BaseAddress = { 0x0057B0B8 }; // Currently the playerBaseAddress
+//DWORD baseAddress = 0x0;
 DWORD AmmoOffsets[] = { 0x140, 0x0 };
-int ammoValue = 500;
+DWORD healthPointOffsets[] = { 0xEC, 0x0 };
 
+int ammoValue = 0;
+int healthPointValue = 0;
 
 
 
@@ -35,20 +42,49 @@ int main(void){
 	    system("pause");
 	    return EXIT_FAILURE;
     }
+    ammoValue = 4321;
+	updateAmmo(hProcess, ammoValue);
 
-    DWORD finalPointer = findingFinalPointer(1, hProcess, AmmoOffsets, AmmoBaseAddress);
+	// Healthpoint Read Update
+	healthPointValue = 1234;
+    updateHealthPoint(hProcess, healthPointValue);
 
-    readFromMemory(hProcess, finalPointer);
-
-	ammoValue = 1000;
-
-    writeToMemory (hProcess, finalPointer);
-    
-	readFromMemory(hProcess, finalPointer);
+   
    
 
 }
 
+bool updateAmmo(HANDLE hProcess, int value)
+{
+    // Ammo Value read and update
+	DWORD finalPointer = findingFinalPointer(1, hProcess, AmmoOffsets, BaseAddress);
+
+	printf("Reading Ammo Value .... \n");
+    readFromMemory(hProcess, finalPointer);
+
+	printf("Updating Ammo Value .... \n");
+	writeToMemory(hProcess, finalPointer,value);
+    
+	printf("Reading Ammo Value .... \n");
+	readFromMemory(hProcess, finalPointer);
+
+	printf("\n\n");
+}
+
+bool updateHealthPoint(HANDLE hProcess, int value)
+{
+	DWORD finalPointer = findingFinalPointer(1, hProcess, healthPointOffsets, BaseAddress);
+    printf("Reading Health Point Value .... \n");
+    readFromMemory(hProcess, finalPointer);
+	
+	printf("Updating Health Point Value .... \n");
+    writeToMemory(hProcess, finalPointer,value);
+    
+	printf("Reading Health Point Value .... \n");
+	readFromMemory(hProcess, finalPointer);
+
+	printf("\n\n");
+}
 
 // With the help of this address, we can use the name of game to find the ProcessID
 DWORD findingProcessId()
@@ -90,11 +126,13 @@ DWORD findingFinalPointer(int Pointerdepth, HANDLE hProcess, DWORD offsets[], DW
         {
             ReadProcessMemory(hProcess, (LPCVOID)BaseAddress, &tempPointer, sizeof(tempPointer), 0);
             pointerAddress = tempPointer + offsets[i];
+			//printf("The address of final address i=0 %p\n", pointerAddress);
+
         } else {
             ReadProcessMemory(hProcess,(LPCVOID) pointerAddress,&tempPointer,sizeof(tempPointer), 0);
             pointerAddress = tempPointer + offsets[i];
         }
-        printf("The value of i:%d \n",i);
+        //printf("The value of i:%d \n",i);
 	
 	}
 	return pointerAddress;
@@ -109,17 +147,18 @@ bool readFromMemory(HANDLE hProcess, DWORD address)
 	system("pause");
 	return EXIT_FAILURE;
     }
-    printf("The ammoValye is equal to %d \n",rAmmoValue);
+    printf("The value is equal to %d \n",rAmmoValue);
     
 }
 
-bool writeToMemory(HANDLE hProcess, DWORD address)
+bool writeToMemory(HANDLE hProcess, DWORD address, int value)
 {
-    BOOL rpmReturn2 = WriteProcessMemory(hProcess,(LPVOID)address,&ammoValue,sizeof(ammoValue),0);
+    BOOL rpmReturn2 = WriteProcessMemory(hProcess,(LPVOID)address,&value,sizeof(value),0);
     if (rpmReturn2 == FALSE) {
 	cout << "ReadProcessMemory failed. GetLastError = " << dec << GetLastError() << endl;
 	system("pause");
 	return EXIT_FAILURE;
     }
-    printf("The ammoValye updated to %d \n",ammoValue);
+  
+    printf("The value updated to %d \n",value);
 }
